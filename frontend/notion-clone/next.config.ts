51159c85f874ed6c8f97d2402586c087e37437ff
Anubhav Kumar <118300550+AnubhavKumar1133@users.noreply.github.com>
2025-04-09 06:25:13 +0530

    Update next.config.ts

--- frontend/notion-clone/next.config.ts
+++ frontend/notion-clone/next.config.ts
@@ -1,12 +1,8 @@
-// import type { NextConfig } from "next";
+import type { NextConfig } from "next";
 
-// const nextConfig: NextConfig = {
-//   /* config options here */
-// };
-
-// export default nextConfig;
-const nextConfig: any = {
-  // config options here
+const nextConfig: NextConfig = {
+  /* config options here */
 };
 
 export default nextConfig;
+
